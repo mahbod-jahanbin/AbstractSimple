1f208ee934e5716f507e762a436bf72fa1bd6e2e
mahbod.jahanbin@gmail.com <mahbod.jahanbin@gmail.com>
2023-10-31 12:49:53 +0330

    Add project files.

---
+++ AbstractSimple/MyDerivedClass.cs
@@ -0,0 +1,14 @@
+﻿using AbstractSimple;
+
+public class MyDerivedClass : SimpleAbstract
+{
+    public override void MyAbstractMethod()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string MyMahbodAbstractMethod()
+    {
+        return "mahbod";
+    }
+}
---
+++ AbstractSimple/SimpleAbstract.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractSimple
+{
+    public abstract class SimpleAbstract
+    {
+        public abstract void MyAbstractMethod();
+
+        public abstract string MyMahbodAbstractMethod();
+        public void MyMethod()
+        { 
+
+        }
+    }
+}
